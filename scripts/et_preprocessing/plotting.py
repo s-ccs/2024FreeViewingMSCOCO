a4f2cc7fb7ed9f684b0b3ea648a4069016c97feb
chrissibohnacker <92847672+chrissibohnacker@users.noreply.github.com>
2026-04-16 16:20:17 +0200

    new fcts

--- scripts/et_preprocessing/plotting.py
+++ scripts/et_preprocessing/plotting.py
@@ -111,8 +111,6 @@ def plot_main_sequence(
     suffix = []
     if drop_near_blinks:
         suffix.append("blink-cleaned")
-    if drop_ms_outliers:
-        suffix.append(f"outliers-dropped (>{ms_outlier_mad_thresh} MAD)")
     ax.set_title(
         f"{title} — {title_map[by_eye]}" + (f" ({', '.join(suffix)})" if suffix else "")
     )
@@ -128,7 +126,7 @@ def plot_main_sequence(
         )
 
 
-# Saccade Amplitude
+# Fixation Duration
 # =============================================================================
 def plot_fixation_duration(
     events_df: pd.DataFrame,
@@ -144,17 +142,15 @@ def plot_fixation_duration(
 
     Args:
         events_df (pd.DataFrame):
-        out_path (str): Directory to save the figure. Pass None to skip saving.
+        out_path (str): Directory to save the figure. Pass None to skip saving. # TBD
         out_file_format (str): File extension for saving, e.g. 'svg', 'pdf', 'eps'
         by_eye (str): One of: 'all', 'left', 'right', 'both'
         min_ms (float, optional): lower bound to drop ultra-short blinks/micro-fixations
         max_ms (float, optional): upper bound to drop implausibly long fixations #TBD ist das überhaupt noch notwendig mit preprocessing?
         title (str, optional): Defaults to 'Fixation Durations'.
     Raises:
-        ValueError: HIERHIERHIERHIEHR
-        ValueError: _description_
+        ValueError: No fixation durations within min_ms - max_ms found
     """
-HIERHIERHEIRHEIRHEIRHEIRHEI
     if by_eye not in {"all", "left", "right", "both"}:
         raise ValueError("by_eye must be one of: 'all', 'left', 'right', 'both'")
 
@@ -177,6 +173,14 @@ HIERHIERHEIRHEIRHEIRHEIRHEI
     if dur.empty:
         raise ValueError(
             "No fixation durations post filtering. Check inputs or ranges."
+        )
+    else:  # tBD: verbatim?
+        # Report counts
+        dropouts = len(fix["duration_ms"]) - len(dur)
+        print(f"Total fixations: {len(fix["duration_ms"])}")
+        print(f"Kept fixations ({min_ms}ms <= duration <= {max_ms}ms): {len(dur)}")
+        print(
+            f"Dropped outliers: {dropouts}, {(dropouts/len(fix["duration_ms"]))*100:.2f}%"
         )
 
     fig, ax = plt.subplots()
@@ -202,3 +206,211 @@ HIERHIERHEIRHEIRHEIRHEIRHEI
     print(
         f"Plot saved to file '{out_path}/{title.lower().replace(' ', '_')}-{by_eye}Eyes.{out_file_format}'"
     )
+
+
+# Saccade Amplitude
+# =============================================================================
+def plot_saccade_amplitude(
+    events_df: pd.DataFrame,
+    out_path: str,
+    out_file_format: str = OUT_FILE_FORMAT,
+    by_eye: str = BY_EYE,
+    max_deg: float = SACC_AMP_MAX_DEG,
+    title: str = "Saccade Amplitude",
+):
+    """
+    Histogram of saccade amplitudes (degrees), outliers dropped (upper bound max_deg).
+
+    Args:
+        events_df (pd.DataFrame): Event dataframe containing a 'trial_type' column with saccade events.
+        out_path (str): Directory to save the figure. Pass None to skip saving.
+        out_file_format (str): File extension for saving, e.g. 'svg', 'pdf', 'eps'
+        by_eye (str): One of: 'all', 'left', 'right', 'both'
+        max_deg (float, optional): Upper bound to drop implausibly large saccade amplitudes.
+        title (str, optional): Defaults to 'Saccade Amplitude'.
+    """
+
+    s_df = events_df[events_df["trial_type"] == "saccade"].copy()
+
+    # 1) Filter by eye
+    if by_eye != "all":
+        eye_mapping = {"left": "L", "right": "R", "both": "both"}
+        chosen_eye = eye_mapping[by_eye]
+        s_df = s_df.query("eye == @chosen_eye").copy()
+
+    # 2) Select saccade amplitudes in degrees
+    # amplitudes = df["amplitude_deg"].dropna()
+    all_amplitudes = s_df["sacc_visual_angle"].dropna()
+    if max_deg is not None:
+        amplitudes = all_amplitudes[all_amplitudes <= max_deg]
+
+    # Identify dropped outliers
+    dropout = len(all_amplitudes[all_amplitudes > max_deg])
+
+    # Report counts
+    # TBD: verbatim?
+    print(f"Total saccades: {len(all_amplitudes)}")
+    print(f"Kept saccades (<={max_deg}°): {len(amplitudes)}")
+    print(
+        f"Dropped outliers (>{max_deg}°): {dropout}, {(dropout/len(all_amplitudes))*100:.2f}%"
+    )
+
+    # 3) Create figure
+    fig, ax = plt.subplots(figsize=(5, 4))
+
+    ax.hist(amplitudes, bins=40, edgecolor="black")
+
+    # 4) Labels & title
+    title_map = {
+        "all": "All eyes",
+        "left": "Left eye only",
+        "right": "Right eye only",
+        "both": "Binocular only",
+    }
+
+    ax.set_title(f"{title} — {title_map[by_eye]}")
+    ax.set_xlabel("Saccade amplitude (deg)")
+    ax.set_ylabel("Count")
+    ax.set_xlim(left=0)
+
+    fig.tight_layout()
+
+    # 5) Save & show
+    out_file = (
+        f"{out_path}/{title.lower().replace(' ', '_')}-{by_eye}Eyes.{out_file_format}"
+    )
+    fig.savefig(out_file, bbox_inches="tight")
+    print(f"Plot saved to file '{out_file}'")
+    plt.show()
+    plt.close(fig)
+
+
+# Saccade Duration
+# =============================================================================
+def plot_saccade_duration(
+    events_df: pd.DataFrame,
+    out_path: str,
+    out_file_format: str = OUT_FILE_FORMAT,
+    by_eye: str = BY_EYE,
+    title: str = "Saccade Duration",
+    max_dur: int = SACC_DUR_MAX_MS,
+):
+    """
+    Histogram of fixation frequency (fixations per second).
+
+    Args:
+        events_df (pd.DataFrame):
+        out_path (str): Directory to save the figure. Pass None to skip saving.
+        out_file_format (str, optional):  File extension for saving, e.g. 'svg', 'pdf', 'eps'. Defaults to OUT_FILE_FORMAT.
+        by_eye (str, optional): One of: 'all', 'left', 'right', 'both'. Defaults to BY_EYE.
+        title (str, optional): Defaults to "Saccade Duration".
+        max_dur (int, optional): _description_. Defaults to SACC_DUR_MAX_MS.
+    """
+
+    s_df = events_df[events_df["trial_type"] == "saccade"].copy()
+
+    # 1) Filter by eye
+    if by_eye != "all":
+        eye_mapping = {"left": "L", "right": "R", "both": "both"}
+        chosen_eye = eye_mapping[by_eye]
+        s_df = s_df.query("eye == @chosen_eye")
+
+    # 2) Convert duration from seconds to milliseconds
+    durations = (s_df["duration"] * 1000).dropna()
+    # Report counts
+    # TBD: verbatim?
+    print(f"Total saccades: {len(durations)}")
+
+    # 3) Drop saccades >120ms
+    if max_dur is not None:
+        durations = durations[durations <= max_dur]
+        durations_copy = durations.copy()
+        dropout = len(durations_copy[durations > max_dur])
+        # Report counts
+        # TBD: verbatim?
+        print(f"Kept saccades (<={max_dur}°): {len(durations)}")
+        print(
+            f"Dropped {(dropout/len(durations))*100:.2f}% samples of duration > {max_dur} milliseconds."
+        )
+
+    # 4) Create figure
+    fig, ax = plt.subplots(figsize=(5, 4))
+
+    ax.hist(durations, bins=40, edgecolor="black")
+
+    # 5) Labels & title
+    title_map = {
+        "all": "All eyes",
+        "left": "Left eye only",
+        "right": "Right eye only",
+        "both": "Binocular only",
+    }
+
+    ax.set_title(f"{title} — {title_map[by_eye]}")
+    ax.set_xlabel("Saccade duration (ms)")
+    ax.set_ylabel("Count")
+    ax.set_xlim(left=0)
+
+    fig.tight_layout()
+
+    # 6) Save pre show
+    # TBD: Overwrite Flag/None/Show
+    out_file = (
+        f"{out_path}/{title.lower().replace(' ', '_')}-{by_eye}Eyes.{out_file_format}"
+    )
+    fig.savefig(out_file, bbox_inches="tight")
+    print(f"Plot saved to file '{out_file}'")
+
+    plt.show()
+    plt.close(fig)
+
+
+# Fixation Frequency
+# =============================================================================
+
+
+def plot_fixation_frequency(
+    events_df: pd.DataFrame,
+    out_path: str,
+    out_file_format: str = OUT_FILE_FORMAT,
+    by_eye: str = BY_EYE,
+    title="Fixation frequency histogram",
+):
+    """
+    Histogram of fixation frequency (fixations per second), binned by second-level onset buckets.
+
+    Args:
+        events_df (pd.DataFrame): Event dataframe containing a 'trial_type' column with fixation events.
+        out_path (str): Directory to save the figure. Pass None to skip saving.
+        out_file_format (str): File extension for saving, e.g. 'svg', 'pdf', 'eps'.
+        by_eye (str): One of: 'all', 'left', 'right', 'both'
+        title (str, optional): Defaults to 'Fixation frequency histogram'.
+    """
+    f_df = events_df[events_df["trial_type"] == "fixation"].copy()
+
+    if by_eye != "all":
+        eye_mapping = {"left": "L", "right": "R", "both": "both"}
+        chosen_eye = eye_mapping[by_eye]
+        f_df = f_df.query("eye == @chosen_eye").copy()
+
+    f_df["sec"] = f_df["onset"].astype(float).floordiv(1).astype(int)
+    fix_per_sec = f_df.groupby("sec").size()
+
+    plt.figure()
+    plt.hist(
+        fix_per_sec.values,
+        bins=np.arange(fix_per_sec.max() + 2) - 0.3,
+        width=0.6,
+    )
+    plt.xlim(left=-0.3)
+    plt.xlabel("Fixation Frequency (per Second)")
+    plt.ylabel("Count")
+    plt.title(title)
+
+    # TBD: Overwrite Flag/None/Show
+    out_file = (
+        f"{out_path}/{title.lower().replace(' ', '_')}-{by_eye}Eyes.{out_file_format}"
+    )
+    plt.savefig(out_file, bbox_inches="tight")
+    print(f"Plot saved to '{out_file}'")
+    plt.show()
